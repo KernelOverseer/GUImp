a3d940710137bf9247b2b94ba336951b48ffcc5e
KernelOverseer <kerneloverseer@gmail.com>
2021-07-12 21:22:03 +0100

    commented some css code and added it to a new branch to avoid compilation problems

--- sources/style_sheet/css_to_raw_style.c
+++ sources/style_sheet/css_to_raw_style.c
@@ -6,7 +6,7 @@
 /*   By: abiri <kerneloverseer@pm.me>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 02:50:29 by abiri             #+#    #+#             */
-/*   Updated: 2021/07/12 21:19:41 by abiri            ###   ########.fr       */
+/*   Updated: 2021/07/12 21:21:37 by abiri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,25 +42,25 @@ t_libui_raw_style   css_to_raw_style(t_list_head props)
     }
 }
 
-int                 css_apply_to_query(t_libui_window *widnow, t_css_element *element)
-{
+// int                 css_apply_to_query(t_libui_window *widnow, t_css_element *element)
+// {
     
-}
+// }
 
-int                 css_apply_to_element(t_libui_component *component, t_css_element)
-{
+// int                 css_apply_to_element(t_libui_component *component, t_css_element)
+// {
     
-}
+// }
 
-int                 css_apply_to_window(t_libui_window *window, t_css_context *css)
-{
-    t_list_head     elements;
-    t_css_element   *element;
+// int                 css_apply_to_window(t_libui_window *window, t_css_context *css)
+// {
+//     t_list_head     elements;
+//     t_css_element   *element;
 
-    elements = css->elements;
-    elements.iterator = elements.first;
-    while ((element = ttslist_iter_content(&elements)))
-    {
+//     elements = css->elements;
+//     elements.iterator = elements.first;
+//     while ((element = ttslist_iter_content(&elements)))
+//     {
 
-    }
-}
+//     }
+// }
